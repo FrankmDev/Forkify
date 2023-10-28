c94066b7cf3caf4fbb670f91b3a3e04a50c3b181
FrankmDev <frankmmarindev@gmail.com>
2023-10-28 14:31:57 +0200

    Forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd544e0a1-5e9a-456f-8ed7-7d41e87bb42d';
+export const MODAL_CLOSE_SEC = 2.5;
